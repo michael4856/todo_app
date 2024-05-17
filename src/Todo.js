5faaa0d56a8e76788e0fc0732b3a6869ee73b431
michael gebre <michaelgebregorgis@gmail.com>
2024-05-17 21:23:11 +0300

    modified todo

--- src/Todo.js
+++ src/Todo.js
@@ -3,6 +3,21 @@ import './todo.css'
 export default function Todo(){
     const [taskList, setTaskList] = useState(["Goto church", "Eat breakfast", "music class"])
     const [task, setTask] = useState()
+
+    function onChangeHandler(e){
+        setTask(e.target.value)
+    }
+    function addHandler(){
+        const trimmed = task.replace(/\s+/g, '')
+        if(trimmed !== ""){
+        setTaskList(t=> [...t, task] )
+        }
+        setTask("")
+    }
+    function Delete(index){
+        const updated = taskList.filter((_, i) => i !== index)
+        setTaskList(updated)
+    }
     return <div>
         <h1>Welcome to this page!</h1>
         <h2>Todo list App</h2>
@@ -11,8 +26,8 @@ export default function Todo(){
         <p>List your tasks here</p>
 
         <div className="input-div">
-            <input className="input" type="text" value={task} placeholder="what's your next plan.." />
-            <button>Add</button>
+            <input className="input" type="text" value={task} onChange={onChangeHandler} placeholder="what's your next plan.." />
+            <button onClick={addHandler}>Add</button>
         </div>
 
         <div>
@@ -20,7 +35,7 @@ export default function Todo(){
                 <ul className="list-style" key={index}>
                    <span className="list"> {i} </span>
                    <button>Edit</button>
-                   <button>Delete</button>
+                   <button value={task} onClick={Delete}>Delete</button>
                 </ul>
            )}
         </div>
